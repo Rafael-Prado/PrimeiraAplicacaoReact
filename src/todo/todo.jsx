abd6279f35fc123cbb5eb036bfb6a6493e0de6c5
Rafael da cruz do prado <rafaeldacruzdoprado@hotmail.com>
2018-08-26 20:11:26 -0300

    Adicinado método de pesquisa e cadastro

--- src/todo/todo.jsx
+++ src/todo/todo.jsx
@@ -1,15 +1,47 @@
 import React, { Component } from 'react'
+import axios from 'axios'
 
 import PageHeader from '../template/pageHeader'
 import TodoForm from './todoForm'
 import TodoList from './todoList';
 
+const URL = 'http://localhost:3003/api/todos'
+
 export default class Todo extends Component {
+
+    constructor(props) {
+        super(props)
+        this.state = { description: '', list: []}
+
+        this.handleChange = this.handleChange.bind(this)
+        this.handleAdd = this.handleAdd.bind(this)
+
+        this.refresh()
+    }
+
+    refresh(){
+        axios.get(`${URL}?sort=-createdAt`)
+            .then(resp => console.log(resp.data))
+    }
+
+    handleChange (e){
+        this.setState({...this.state, description: e.target.value })
+    }
+
+    handleAdd() {
+        const descripion = this.state.description
+        axios.post(URL, { descripion })
+            .then(resp => console.log('funcionou!'))
+    }
+
     render (){
         return(
             <div> 
                 <PageHeader name='Tarefas' small='Cadastro'></PageHeader>
-                <TodoForm></TodoForm>
+                <TodoForm
+                    description ={ this.state.description  }
+                    handleChange={this.handleChange }
+                    handleAdd={ this.handleAdd }/>
                 <TodoList></TodoList>
             </div>
         )
